747169580452e43ae1d6a9116c2da1dd8093b568
PersonalStuffs <PersonalStuffs@users.noreply.github.com>
2020-09-06 17:44:27 -0400

    updated code

--- add.go
+++ add.go
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-package main
-
-import "fmt"
-
-func main() {
-
-	fmt.Print(add(12, 3))
-}
-
-func add(a, b int) int {
-
-	return a + b
-
-}
-=======
 package main
 
 import "fmt"
@@ -28,4 +12,3 @@ func add(a, b int) int {
 	return a + b
 
 }
->>>>>>> e38f169f5e4198594313145f866161dd4642b97a
