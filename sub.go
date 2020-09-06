8ca8337d5a0944e55ae26261b617c69e816e2b98
PersonalStuffs <PersonalStuffs@users.noreply.github.com>
2020-09-06 17:43:03 -0400

    sub fixed

--- sub.go
+++ sub.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main
 
 import "fmt"
@@ -13,19 +12,3 @@ func sub(a, b int) int {
 	return a - b
 
 }
-=======
-package main
-
-import "fmt"
-
-func main() {
-
-	fmt.Print(sub(2, 3))
-}
-
-func sub(a, b int) int {
-
-	return a - b
-
-}
->>>>>>> e38f169f5e4198594313145f866161dd4642b97a
